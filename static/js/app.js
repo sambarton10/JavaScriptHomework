ba33b7c6536c09116ee720c16660546d8a2c2a11
sambarton10 <69605114+sambarton10@users.noreply.github.com>
2021-01-02 19:37:37 -0600

    Was able complete level 1

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,41 @@
 // from data.js
 var tableData = data;
+var button = d3.select("#filter-btn");
+var form = d3.select("#datetime");
 
-var thead= d3.select("thead");
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
 
-//Checking to see if data import works
-console.log(tableData);
+// Complete the event handler function for the form
+function runEnter() {
 
-tableData.forEach(function(ufoReport) {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  
+  console.log(inputValue);
+  
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+  console.log(filteredData);
+
+  var thead= d3.select("thead");
+    //Checking to see if data import works
+  filteredData.forEach(function(ufoReport) {
   console.log(ufoReport);
-  var row = thead.append("tr");
+  var row = thead.append("tr")
 
-Object.entries(ufoReport).forEach(function([key, value]) {
-    console.log(key, value);
-    var cell = row.append("td");
-    cell.text(value);
+  Object.entries(ufoReport).forEach(function([key, value]) {
+  console.log(key, value);
+  var cell = row.append("td");
+  cell.text(value);
   });
 
 });
-
-
+};
